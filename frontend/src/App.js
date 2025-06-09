9c0c27dc1d875aa3ff9924e5dbb1d1fd7c45be26
gruppo23-code <gruppo39poliba@gmail.com>
2025-06-09 15:23:56 +0200

    Navbar con material

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,14 @@
-import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
 
-function App() {
-  const [utenti, setUtenti] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:5001/api/utenti')
-        .then(res => res.json())
-        .then(data => {
-          console.log('Utenti ricevuti:', data); // stampa in console
-          setUtenti(data); // aggiorna lo stato
-        })
-        .catch(err => console.error('Errore nel fetch:', err));
-  }, []);
-
-  return (
-      <div>
-        <h1>Lista utenti (test)</h1>
-        <ul>
-          {utenti.map((utente, index) => (
-              <li key={utente._id || index}>
-                {utente.nome} {utente.cognome} - {utente.email}
-              </li>
-          ))}
-        </ul>
-      </div>
-  );
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Menu" element={<Menu />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
-export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+    return <h1>Benvenuto in Meat-N-Cheese!</h1>;
+}
