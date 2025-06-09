48ac930ef4f4afe04e40db3d2762c0fe747c2fe7
gruppo23-code <gruppo39poliba@gmail.com>
2025-06-09 16:26:06 +0200

    Prime modifiche Navbar

--- frontend/src/compontents/Navbar.jsx
+++ frontend/src/compontents/Navbar.jsx
@@ -36,26 +36,17 @@ function ResponsiveAppBar() {
     };
 
     return (
-        <AppBar position="static">
-            <Container maxWidth="xl">
+        <AppBar position="static" sx={{ bgcolor: '#7A1F26' }}>
+        <Container maxWidth="xl">
                 <Toolbar disableGutters>
-                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+
                     <Typography
                         variant="h6"
                         noWrap
                         component="a"
-                        href="#app-bar-with-responsive-menu"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
+                        href="/"
                     >
-                        LOGO
+                        <img src="/images/Logo_NoBg.png" alt="Logo" style={{ height: 45 }} />
                     </Typography>
 
                     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
